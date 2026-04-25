b4fc0da70079409caaa1194505b70143e94feaa3
CarmenVavra <carmen.vavra@chello.at>
2026-04-26 01:24:11 +0200

    fix: added TakeawayService

--- src/main/java/com/cartoni/bucketlist/takeaway/TakeawayController.java
+++ src/main/java/com/cartoni/bucketlist/takeaway/TakeawayController.java
@@ -1,88 +1,40 @@
 package com.cartoni.bucketlist.takeaway;
 
-import com.cartoni.bucketlist.activity.ActivityRepository;
-import com.cartoni.bucketlist.activityTakeaway.ActivityTakeaway;
-import com.cartoni.bucketlist.activityTakeaway.ActivityTakeawayRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 public class TakeawayController {
 
-    @Autowired
-    private ActivityRepository activityRepository;
+    private final TakeawayService takeawayService;
 
-    @Autowired
-    private TakeawayRepository takeawayRepository;
-
-    @Autowired
-    private ActivityTakeawayRepository activityTakeawayRepository;
+    public TakeawayController(TakeawayService takeawayService) {
+        this.takeawayService = takeawayService;
+    }
 
     @GetMapping("/takeaway")
-    public ResponseEntity<Takeaway> getById(Integer id) {
-        try {
-            Optional<Takeaway> takeaway = takeawayRepository.findById(id);
-            if(takeaway.isPresent()) {
-                return new ResponseEntity<>(takeaway.get(), HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Takeaway> getById(@RequestParam(value = "id") Integer id) {
+        return takeawayService.findByTakeawayId(id);
     }
 
     @GetMapping("/takeaway/user")
     public ResponseEntity<Iterable<Takeaway>> getAllByUserId(@RequestParam(value = "userId") Integer userId) {
-        try {
-            Iterable<Takeaway> takeaway = takeawayRepository.findByUserId(userId);
-            if(takeaway != null) {
-                return new ResponseEntity<>(takeaway, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return takeawayService.findAllTakeawayByUserId(userId);
     }
 
     @PostMapping("/takeaway/create")
     public ResponseEntity<Takeaway> create(@RequestBody Takeaway takeaway,
                                            @RequestParam Integer activityId) {
-        try{
-            Takeaway createdTakeaway = takeawayRepository.save(takeaway);
-
-            ActivityTakeaway activityTakeaway = new ActivityTakeaway();
-            activityTakeaway.setActivityId(activityId);
-            activityTakeaway.setTakeawayId(createdTakeaway.getId());
-            ActivityTakeaway cratedActivityTakeaway = activityTakeawayRepository.save(activityTakeaway);
-
-            return new ResponseEntity<>(createdTakeaway, HttpStatus.CREATED);
-            }catch(Exception e) {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
+        return takeawayService.createTakeaway(takeaway, activityId);
     }
 
     @PutMapping("/takeaway")
     public ResponseEntity<Takeaway> update(@RequestBody Takeaway takeaway) {
-        try {
-            takeawayRepository.save(takeaway);
-            return new ResponseEntity<>(takeaway, HttpStatus.OK);
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        return takeawayService.updateTakeaway(takeaway);
     }
 
     @DeleteMapping("/takeaway")
     public ResponseEntity<Takeaway> delete(@RequestParam(value = "id") Integer id) {
-        try {
-            takeawayRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return takeawayService.deleteTakeaway(id);
     }
-
 }
---
+++ src/main/java/com/cartoni/bucketlist/takeaway/TakeawayService.java
@@ -0,0 +1,70 @@
+package com.cartoni.bucketlist.takeaway;
+
+import com.cartoni.bucketlist.activityTakeaway.ActivityTakeaway;
+import com.cartoni.bucketlist.activityTakeaway.ActivityTakeawayRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+@Service
+public class TakeawayService {
+    private final TakeawayRepository takeawayRepository;
+    private final ActivityTakeawayRepository activityTakeawayRepository;
+
+    public TakeawayService(TakeawayRepository takeawayRepository, ActivityTakeawayRepository activityTakeawayRepository) {
+        this.takeawayRepository = takeawayRepository;
+        this.activityTakeawayRepository = activityTakeawayRepository;
+    }
+
+    public ResponseEntity<Takeaway> findByTakeawayId(Integer id) {
+        try {
+            return new ResponseEntity<>(takeawayRepository.findById(id).get(), HttpStatus.OK);
+        }catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/takeaway/user")
+    public ResponseEntity<Iterable<Takeaway>> findAllTakeawayByUserId(Integer userId) {
+        try {
+            return new ResponseEntity<>(takeawayRepository.findByUserId(userId), HttpStatus.OK);
+        }catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/takeaway/create")
+    public ResponseEntity<Takeaway> createTakeaway(Takeaway takeaway, Integer activityId) {
+        try{
+            Takeaway createdTakeaway = takeawayRepository.save(takeaway);
+
+            ActivityTakeaway activityTakeaway = new ActivityTakeaway();
+            activityTakeaway.setActivityId(activityId);
+            activityTakeaway.setTakeawayId(createdTakeaway.getId());
+            activityTakeawayRepository.save(activityTakeaway);
+
+            return new ResponseEntity<>(createdTakeaway, HttpStatus.CREATED);
+        }catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<Takeaway> updateTakeaway(Takeaway takeaway) {
+        try {
+            takeawayRepository.save(takeaway);
+            return new ResponseEntity<>(takeaway, HttpStatus.OK);
+        }catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<Takeaway> deleteTakeaway(Integer id) {
+        try {
+            takeawayRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
