d29f1a73bfd147c425f1dada695bfd96fc194561
CarmenVavra <carmen.vavra@chello.at>
2026-04-25 15:42:30 +0200

    fix: added FantasyService

--- src/main/java/com/cartoni/bucketlist/fantasy/FantasyController.java
+++ src/main/java/com/cartoni/bucketlist/fantasy/FantasyController.java
@@ -10,166 +10,76 @@ import java.util.Optional;
 @RestController
 public class FantasyController {
 
-    @Autowired
-    private FantasyRepository fantasyRepository;
+    private final FantasyService fantasyService;
+
+    public FantasyController(FantasyService fantasyService) {
+        this.fantasyService = fantasyService;
+    }
 
     @GetMapping("/fantasy")
-    public ResponseEntity<Fantasy> getById(Integer id) {
-        try {
-            Optional<Fantasy> fantasy = fantasyRepository.findById(id);
-            if(fantasy.isPresent()) {
-                return new ResponseEntity<>(fantasy.get(), HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Fantasy> getById(@RequestParam (value = "id") Integer id) {
+        return fantasyService.findFantasyById(id);
     }
 
     @GetMapping("/fantasy/private")
     public ResponseEntity<Iterable<Fantasy>> getAllByUserId(@RequestParam(value = "userId") Integer userId) {
-        try {
-            Iterable<Fantasy> fantasy = fantasyRepository.findByUserIdAndAcceptedAndDeniedAndDone(userId, false, false, false );
-            if(fantasy != null) {
-                return new ResponseEntity<>(fantasy, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return  fantasyService.findAllFantasyByUserId(userId);
     }
 
     @GetMapping("/fantasy/public")
     public ResponseEntity<Iterable<Fantasy>> getAllByPublished() {
-        try {
-            Iterable<Fantasy> fantasys = fantasyRepository.findByPublished(true);
-            if(fantasys != null) {
-                return new ResponseEntity<>(fantasys, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return fantasyService.findAllFantasyByPublished();
     }
 
     @GetMapping("/fantasy/byAccepted")
     public ResponseEntity<Iterable<Fantasy>> getAllByAccepted(@RequestParam(value = "userId") Integer userId) {
-        try {
-            Iterable<Fantasy> fantasy = fantasyRepository.findByUserIdAndAccepted(userId, true);
-            if(fantasy != null) {
-                return new ResponseEntity<>(fantasy, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return fantasyService.findAllFantasyByAccepted(userId);
     }
 
     @GetMapping("/fantasy/byDenied")
     public ResponseEntity<Iterable<Fantasy>> getAllByDenied(@RequestParam(value = "userId") Integer userId) {
-        try {
-            Iterable<Fantasy> fantasy = fantasyRepository.findByUserIdAndDenied(userId, true);
-            if(fantasy != null) {
-                return new ResponseEntity<>(fantasy, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return  fantasyService.findAllFantasyByDenied(userId);
     }
 
     @GetMapping("/fantasy/byDone")
     public ResponseEntity<Iterable<Fantasy>> getAllByDone(@RequestParam(value = "userId") Integer userId) {
-        try {
-            Iterable<Fantasy> fantasy = fantasyRepository.findByUserIdAndDone(userId, true);
-            if(fantasy != null) {
-                return new ResponseEntity<>(fantasy, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return fantasyService.findAllFantasyByDone(userId);
     }
 
     @PostMapping("/fantasy")
     public ResponseEntity<Fantasy> create(@RequestBody Fantasy fantasy) {
-        try {
-            fantasyRepository.save(fantasy);
-            return new ResponseEntity<>(fantasy, HttpStatus.CREATED);
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        return fantasyService.createFantasy(fantasy);
     }
 
     @PutMapping("/fantasy")
     public ResponseEntity<Fantasy> update(@RequestBody Fantasy fantasy) {
-        try {
-            fantasyRepository.save(fantasy);
-            return new ResponseEntity<>(fantasy, HttpStatus.OK);
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        return fantasyService.updateFantasy(fantasy);
     }
 
     @PatchMapping("/fantasy/togglePublished")
     public ResponseEntity<Fantasy> setPublished(@RequestParam(value = "published") boolean published,
                                                @RequestParam(value = "id") Integer id) {
-        Optional<Fantasy> fantasyItem = fantasyRepository.findById(id);
-        if(fantasyItem.isPresent()){
-            fantasyItem.get().setPublished(published);
-            Fantasy updatedFantasyItem = fantasyRepository.save(fantasyItem.get());
-            return new ResponseEntity<>(updatedFantasyItem, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return fantasyService.setFantasyPublished(published, id);
     }
 
     @PatchMapping("/fantasy/setAccepted")
     public ResponseEntity<Fantasy> setAccepted(@RequestParam(value = "id") Integer id) {
-        Optional<Fantasy> fantasyItem = fantasyRepository.findById(id);
-        if(fantasyItem.isPresent()){
-            fantasyItem.get().setAccepted(true);
-            fantasyItem.get().setDenied(false);
-            Fantasy updatedFantasyItem = fantasyRepository.save(fantasyItem.get());
-            return new ResponseEntity<>(updatedFantasyItem, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return fantasyService.setFantasyAccepted(id);
     }
 
     @PatchMapping("/fantasy/setDenied")
     public ResponseEntity<Fantasy> setDenied(@RequestParam(value = "id") Integer id) {
-        Optional<Fantasy> fantasyItem = fantasyRepository.findById(id);
-        if(fantasyItem.isPresent()){
-            fantasyItem.get().setDenied(true);
-            fantasyItem.get().setAccepted(false);
-            fantasyItem.get().setPublished(false);
-            Fantasy updatedFantasyItem = fantasyRepository.save(fantasyItem.get());
-            return new ResponseEntity<>(updatedFantasyItem, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return fantasyService.setFantasyDenied(id);
     }
 
     @PatchMapping("/fantasy/setDone")
     public ResponseEntity<Fantasy> setDone(@RequestParam(value = "id") Integer id) {
-        Optional<Fantasy> fantasyItem = fantasyRepository.findById(id);
-        if(fantasyItem.isPresent()){
-            fantasyItem.get().setDone(true);
-            fantasyItem.get().setAccepted(false);
-            fantasyItem.get().setPublished(false);
-            fantasyItem.get().setDenied(false);
-            Fantasy updatedFantasyItem = fantasyRepository.save(fantasyItem.get());
-            return new ResponseEntity<>(updatedFantasyItem, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return fantasyService.setFantasyDone(id);
     }
 
     @DeleteMapping("/fantasy")
     public ResponseEntity<Fantasy> delete(@RequestParam(value = "id") Integer id) {
-        try {
-            fantasyRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return fantasyService.deleteFantasy(id);
     }
 
 }
