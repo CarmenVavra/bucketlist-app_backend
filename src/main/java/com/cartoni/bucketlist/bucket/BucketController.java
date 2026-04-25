fe2fb5a454ccd7a46687398e0e34dbedbeac588d
CarmenVavra <carmen.vavra@chello.at>
2026-04-25 16:41:44 +0200

    fix: added BucketService

--- src/main/java/com/cartoni/bucketlist/bucket/BucketController.java
+++ src/main/java/com/cartoni/bucketlist/bucket/BucketController.java
@@ -11,165 +11,75 @@ import java.util.Optional;
 public class BucketController {
 
     @Autowired
-    private BucketRepository bucketRepository;
+    private final BucketService bucketService;
+
+    public BucketController(BucketService bucketService) {
+        this.bucketService = bucketService;
+    }
 
     @GetMapping("/bucketlist")
-    public ResponseEntity<Bucket> getById(Integer id) {
-        try {
-            Optional<Bucket> bucket = bucketRepository.findById(id);
-            if(bucket.isPresent()) {
-                return new ResponseEntity<>(bucket.get(), HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Bucket> getById(@RequestParam(value = "id") Integer id) {
+        return bucketService.findBucketById(id);
     }
 
     @GetMapping("/bucketlist/private")
     public ResponseEntity<Iterable<Bucket>> getAllByUserId(@RequestParam(value = "userId") Integer userId) {
-        try {
-            Iterable<Bucket> bucket = bucketRepository.findByUserIdAndAcceptedAndDeniedAndDone(userId, false, false, false);
-            if(bucket != null) {
-                return new ResponseEntity<>(bucket, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return  bucketService.findAllBucketByUserId(userId);
     }
-
 
     @GetMapping("/bucketlist/public")
     public ResponseEntity<Iterable<Bucket>> getAllByPublished() {
-        try {
-            Iterable<Bucket> buckets = bucketRepository.findByPublished(true);
-            if(buckets != null) {
-                return new ResponseEntity<>(buckets, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return bucketService.findAllBucketByPublished();
     }
 
     @GetMapping("/bucketlist/byAccepted")
     public ResponseEntity<Iterable<Bucket>> getAllByAccepted(@RequestParam(value = "userId") Integer userId) {
-        try {
-            Iterable<Bucket> bucket = bucketRepository.findByUserIdAndAccepted(userId, true);
-            if(bucket != null) {
-                return new ResponseEntity<>(bucket, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return bucketService.findAllBucketByUserIdAndAccepted(userId);
     }
 
     @GetMapping("/bucketlist/byDenied")
     public ResponseEntity<Iterable<Bucket>> getAllByDenied(@RequestParam(value = "userId") Integer userId) {
-        try {
-            Iterable<Bucket> bucket = bucketRepository.findByUserIdAndDenied(userId, true);
-            if(bucket != null) {
-                return new ResponseEntity<>(bucket, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return bucketService.findAllBucketByUserIdAndDenied(userId);
     }
 
     @GetMapping("/bucketlist/byDone")
     public ResponseEntity<Iterable<Bucket>> getAllByDone(@RequestParam(value = "userId") Integer userId) {
-        try {
-            Iterable<Bucket> bucket = bucketRepository.findByUserIdAndDone(userId, true);
-            if(bucket != null) {
-                return new ResponseEntity<>(bucket, HttpStatus.OK);
-            }
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return bucketService.findAllBucketByUserIdAndDone(userId);
     }
 
     @PostMapping("/bucketlist")
     public ResponseEntity<Bucket> create(@RequestBody Bucket bucket) {
-        try {
-            bucketRepository.save(bucket);
-            return new ResponseEntity<>(bucket, HttpStatus.CREATED);
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        return bucketService.createBucket(bucket);
     }
 
     @PutMapping("/bucketlist")
     public ResponseEntity<Bucket> update(@RequestBody Bucket bucket) {
-        try {
-            bucketRepository.save(bucket);
-            return new ResponseEntity<>(bucket, HttpStatus.OK);
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        return bucketService.updateBucket(bucket);
     }
 
     @PatchMapping("/bucketlist/togglePublished")
     public ResponseEntity<Bucket> setPublished(@RequestParam(value = "published") boolean published,
                                                @RequestParam(value = "id") Integer id) {
-        Optional<Bucket> bucketItem = bucketRepository.findById(id);
-        if(bucketItem.isPresent()){
-            bucketItem.get().setPublished(published);
-            Bucket updatedBucketItem = bucketRepository.save(bucketItem.get());
-            return new ResponseEntity<>(updatedBucketItem, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return bucketService.setBucketPublished(published, id);
     }
 
     @PatchMapping("/bucketlist/setAccepted")
     public ResponseEntity<Bucket> setAccepted(@RequestParam(value = "id") Integer id) {
-        Optional<Bucket> bucketItem = bucketRepository.findById(id);
-        if(bucketItem.isPresent()){
-            bucketItem.get().setAccepted(true);
-            bucketItem.get().setDenied(false);
-            Bucket updatedBucketItem = bucketRepository.save(bucketItem.get());
-            return new ResponseEntity<>(updatedBucketItem, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return bucketService.setBucketAccepted(id);
     }
 
     @PatchMapping("/bucketlist/setDenied")
     public ResponseEntity<Bucket> setDenied(@RequestParam(value = "id") Integer id) {
-        Optional<Bucket> bucketItem = bucketRepository.findById(id);
-        if(bucketItem.isPresent()){
-            bucketItem.get().setDenied(true);
-            bucketItem.get().setAccepted(false);
-            bucketItem.get().setPublished(false);
-            Bucket updatedBucketItem = bucketRepository.save(bucketItem.get());
-            return new ResponseEntity<>(updatedBucketItem, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return bucketService.setBucketDenied(id);
     }
 
     @PatchMapping("/bucketlist/setDone")
     public ResponseEntity<Bucket> setDone(@RequestParam(value = "id") Integer id) {
-        Optional<Bucket> bucketItem = bucketRepository.findById(id);
-        if(bucketItem.isPresent()){
-            bucketItem.get().setDone(true);
-            bucketItem.get().setAccepted(false);
-            bucketItem.get().setPublished(false);
-            bucketItem.get().setDenied(false);
-            Bucket updatedBucketItem = bucketRepository.save(bucketItem.get());
-            return new ResponseEntity<>(updatedBucketItem, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return bucketService.setBucketDone(id);
     }
 
     @DeleteMapping("/bucketlist")
     public ResponseEntity<Bucket> delete(@RequestParam(value = "id") Integer id) {
-        try {
-            bucketRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return bucketService.deleteBucket(id);
     }
 }
