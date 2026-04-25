20397686b24c3bae5d8328713cdddc3cc32e33b1
CarmenVavra <carmen.vavra@chello.at>
2026-04-25 16:52:14 +0200

    fix: added authService

--- src/main/java/com/cartoni/bucketlist/auth/AuthenticationController.java
+++ src/main/java/com/cartoni/bucketlist/auth/AuthenticationController.java
@@ -1,8 +1,6 @@
 package com.cartoni.bucketlist.auth;
 
 import com.cartoni.bucketlist.user.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,30 +9,20 @@ import java.util.Optional;
 @RestController
 public class AuthenticationController {
 
-    @Autowired
-    private AuthenticationRepository authenticationRepository;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
     @GetMapping("/auth")
     public ResponseEntity<User> login(@RequestParam(value = "email") String email,
                                       @RequestParam(value = "password") String password) {
-        try{
-            Optional<User> userInDb = Optional.ofNullable(authenticationRepository.findByEmailAndPassword(email, password));
-            if(userInDb.isPresent()) {
-                return new ResponseEntity<>(userInDb.get(), HttpStatus.OK);
-            }
-        }catch(Exception e){
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return authenticationService.login(email, password);
     }
 
     @PostMapping("/auth")
     public ResponseEntity<User> register(@RequestBody User user) {
-        try{
-            User createdUser = authenticationRepository.save(user);
-            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
-        }catch(Exception e){
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
+        return authenticationService.register(user);
     }
 }
